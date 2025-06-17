5dae7b3161267580189003c2d3b3d0130f358d8e
Phuc Dang <phucdang@innoria.com>
2025-06-18 00:21:58 +0700

    feat: add system UUID utilities for Windows, macOS and Linux platforms

--- lib/src/uuid_utils.dart
+++ lib/src/uuid_utils.dart
@@ -1,35 +1,38 @@
 import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 
-class UUIDUtils{
+class UUIDUtils {
   static void openSystemSettings() async {
     if (Platform.isMacOS) {
-      await Process.run('open', ['x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles']);
+      await Process.run('open', [
+        'x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles',
+      ]);
     } else if (Platform.isWindows) {
-      await Process.run('start', ['ms-settings:privacy-customdevices'], runInShell: true);
+      await Process.run('start', [
+        'ms-settings:privacy-customdevices',
+      ], runInShell: true);
     } else if (Platform.isLinux) {
       // Tùy distro, chỉ mở control center cơ bản
       await Process.run('gnome-control-center', []);
     }
   }
 
-  static Future<void> getSystemInfo() async {
+  static Future<Map<String, String>> getSystemInfo() async {
     final deviceInfo = DeviceInfoPlugin();
     if (Platform.isWindows) {
       final info = await deviceInfo.windowsInfo;
-      return {
-        'computerName': info.computerName,
-
-      }
+      return {'computerName': info.computerName};
     } else if (Platform.isMacOS) {
       final info = await deviceInfo.macOsInfo;
       return {
         'computerName': info.computerName,
         'model': info.model,
         'modelName': info.modelName,
-      }
+      };
     } else if (Platform.isLinux) {
       final info = await deviceInfo.linuxInfo;
+      return {'computerName': info.name};
     }
+    throw UnsupportedError('Unsupported platform');
   }
 }
